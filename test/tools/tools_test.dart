24b58785686c0b2950ae92479d7f29ba3ee84e10
Maxime Roucher <maximeroucher@gmail.com>
2025-08-24 10:53:53 +0200

    fix: linter

--- test/tools/tools_test.dart
+++ test/tools/tools_test.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:titan/tools/exception.dart';
 import 'package:titan/tools/functions.dart';
