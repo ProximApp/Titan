9506dbfd776a6469ab6a4fb1f3b1dd4b2b7fb3bf
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2025-09-08 23:53:52 +0200

    fixes

--- lib/feed/providers/is_user_a_member_of_an_association.dart
+++ lib/feed/providers/is_user_a_member_of_an_association.dart
@@ -1,7 +1,7 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:titan/admin/providers/my_association_list_provider.dart';
 
-final isUserAMemberOfAnAssociationProvider = StateProvider<bool>((ref) {
+final isUserAMemberOfAnAssociationProvider = Provider<bool>((ref) {
   final myAssociation = ref.watch(myAssociationListProvider);
   return myAssociation.isNotEmpty;
 });
