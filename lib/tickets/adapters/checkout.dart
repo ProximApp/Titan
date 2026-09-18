db284663c59afe239407e3675460f42a4c95122e
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2026-09-18 09:18:17 +0200

    fix : price in euro conversion

--- lib/tickets/adapters/checkout.dart
+++ lib/tickets/adapters/checkout.dart
@@ -3,5 +3,5 @@ import 'package:titan/generated/openapi.models.swagger.dart'
 
 // Backend stores price in cents; the UI works in euros.
 extension $CheckoutResponse on CheckoutResponse {
-  int get priceInEuros => price ~/ 100;
+  double get priceInEuros => price / 100;
 }
